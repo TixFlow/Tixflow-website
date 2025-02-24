62ba9dfb4a8155ab250ea544d6b7286f1b0cee9b
HaoPhan205 <130329315+HaoPhan205@users.noreply.github.com>
2025-02-24 22:04:23 +0700

    set up

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white text-center py-4 mt-6">
-        © 2024 My App - All Rights Reserved.
-      </footer>
-    );
-  }
-  
